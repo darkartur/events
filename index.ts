c01d805924ab7aba0b4a56db5dc95215fbfe4d06
Arthur <artur.udalov@gmail.com>
2015-03-18 12:53:09 +0300

    multiple callbacks support

--- index.ts
+++ index.ts
@@ -1,16 +1,18 @@
 class Event {
 
     on(handler: () => void): Event {
-        this.handler = handler;
+        this.handlers.push(handler);
         return this;
     }
 
     trigger(): Event {
-        this.handler();
+        this.handlers.forEach((handler: () => void) => {
+            handler();
+        });
         return this;
     }
 
-    private handler: () => void;
+    private handlers: Array<() => void> = [];
 }
 
 export = Event;
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -8,10 +8,33 @@ import Event = require('../index');
 
 describe("events", () => {
 
-    it("on and trigger", (done) => {
-        var event: Event = new Event();
+    var event: Event;
 
+    beforeEach(() => {
+        event = new Event();
+    });
+
+    it("on and trigger", (done) => {
         event.on(done);
         event.trigger();
-    })
+    });
+
+    it("multiple callbacks", () => {
+        var fire_a: boolean = false,
+            fire_b: boolean = false;
+
+        event.on(() => {
+            fire_a = true;
+        });
+
+        event.on(() => {
+            fire_b = true;
+        });
+
+        event.trigger();
+
+        assert.strictEqual(fire_a, true);
+        assert.strictEqual(fire_b, true);
+    });
+
 });
